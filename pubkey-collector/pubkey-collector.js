8003db3f5521005318f97e3d63462784c190ffad
Age <Age@AgeManning.com>
2018-08-15 15:02:17 +1000

    Add batch logic, to correct high loads from hitting the api. This accomodates spam creation of accounts such as in ropsten block: 859550

--- pubkey-collector/pubkey-collector.js
+++ pubkey-collector/pubkey-collector.js
@@ -22,32 +22,32 @@ const https = require('https') // Modify this to http if needed
 
 // Set up the configuration - currently set for the docker containers
 const config = {
-        provider: process.env.IPCADDR,
-        // providerRPC: process.env.RPCADDR, // pub/subs over ipc for now
-        apiHost: process.env.APIHOST,
-        apiPort: process.env.APIPORT,
-        apiKey: process.env.APIKEY,
-        apiPath: "/addkeys",
-        chainId: process.env.CHAINID, // 1 is default for the mainnet.
-        dataStore: "./data/pkcollector.json" // stores current stats
-    }
+  provider: process.env.IPCADDR,
+  // providerRPC: process.env.RPCADDR, // pub/subs over ipc for now
+  apiHost: process.env.APIHOST,
+  apiPort: process.env.APIPORT,
+  apiKey: process.env.APIKEY,
+  apiPath: "/addkeys",
+  chainId: process.env.CHAINID, // 1 is default for the mainnet.
+  dataStore: "./data/pkcollector.json" // stores current stats
+}
 
 // Setup the logging with Winston library
 winston.configure({
-    transports: [
-        new (winston.transports.Console)({
-            timestamp: function() {
-                return moment().tz("Australia/Sydney").format()
-            },
-            formatter: function(options) {
-                return utility.format(
-                    '[%s][%s]: %s',
-                    options.timestamp(),
-                    options.level.toUpperCase(),
-                    options.message)
-            }
-        }),
-    ]
+  transports: [
+    new (winston.transports.Console)({
+      timestamp: function() {
+        return moment().tz("Australia/Sydney").format()
+      },
+      formatter: function(options) {
+        return utility.format(
+          '[%s][%s]: %s',
+          options.timestamp(),
+          options.level.toUpperCase(),
+          options.message)
+      }
+    }),
+  ]
 });
 
 /* Main Function */
@@ -61,34 +61,34 @@ async function main() {
 
   syncEmitter.on("changed", async (syncing)=> {
     if (syncing == undefined) {
-        winston.error("Syncing status undefined. Exiting");
-        return;
+      winston.error("Syncing status undefined. Exiting");
+      return;
     }
     else if (syncing)
-        winston.info("Node is currently syncing. Waiting until sync completes...");
+      winston.info("Node is currently syncing. Waiting until sync completes...");
     else if (!syncing) {
-        winston.info("Syncing Complete. Processing blocks...")
-        await pkCollector.processBlocks()
-        winston.info("Completed searching.");
-        // We are up to date.
-        // Now continue the update process for new blocks.
-        await pkCollector.continualUpdate();
+      winston.info("Syncing Complete. Processing blocks...")
+      await pkCollector.processBlocks()
+      winston.info("Completed searching.");
+      // We are up to date.
+      // Now continue the update process for new blocks.
+      await pkCollector.continualUpdate();
     }
   });
 
   // Check if we are syncing.
   let isSyncing = await pkCollector.isSyncing(); 
   if (!isSyncing) { // not syncing
-      
-      winston.info("Processing blocks...")
-      await pkCollector.processBlocks()
-      winston.info("Completed searching.");
-      // We are up to date.
-      // Now continue the update process for new blocks.
-      await pkCollector.continualUpdate();
+
+    winston.info("Processing blocks...")
+    await pkCollector.processBlocks()
+    winston.info("Completed searching.");
+    // We are up to date.
+    // Now continue the update process for new blocks.
+    await pkCollector.continualUpdate();
   }
   else {
-        winston.info("Node is currently syncing. Waiting until it completes...");
+    winston.info("Node is currently syncing. Waiting until it completes...");
   }
 }
 
@@ -100,128 +100,130 @@ async function main() {
  */
 class PublicKeyCollector {
 
-    constructor(config) {
-        // Set up initial state variables
-        this.currentBlock = 0;
-        this.lastBlock = 0;
-        this.failedBlocks = [];
-        this.transactionTally = 0;
-        this.chainId = config.chainId;
-        this.updateMode = false; // we are not updating single blocks yet.
-        this.web3 = new Web3(config.provider,net);
-        this.dataStore = config.dataStore;
-        this.apiKey = config.apiKey;
-        this.apiOptions = {
-          host: config.apiHost,
-          path: config.apiPath,
-          port: config.apiPort,
-          method: 'PUT'
-        }
+  constructor(config) {
+    // Set up initial state variables
+    this.currentBlock = 0;
+    this.lastBlock = 0;
+    this.failedBlocks = [];
+    this.transactionTally = 0;
+    this.chainId = config.chainId;
+    this.updateMode = false; // we are not updating single blocks yet.
+    this.web3 = new Web3(config.provider,net);
+    this.dataStore = config.dataStore;
+    this.apiKey = config.apiKey;
+    this.apiOptions = {
+      host: config.apiHost,
+      path: config.apiPath,
+      port: config.apiPort,
+      method: 'PUT'
     }
+  }
 
-    /**
-     * Sets up the global database connection and initializes the state
-     * variables.
-     */
-    async initialize() {
-        let blockNumber = await this.web3.eth.getBlockNumber()
-        this.lastBlock = blockNumber;
+  /**
+   * Sets up the global database connection and initializes the state
+   * variables.
+   */
+  async initialize() {
+    let blockNumber = await this.web3.eth.getBlockNumber()
+    this.lastBlock = blockNumber;
 
-        winston.info("Checking for past processing...");
-        if (fs.existsSync(this.dataStore)) {
-          let dataVars = JSON.parse(fs.readFileSync(this.dataStore,'utf8'));
-          // explicitly add them to the class variables 
-          if (dataVars.currentBlock)
-            this.currentBlock = dataVars.currentBlock;
-          if (dataVars.failedBlocks)
-            this.failedBlocks = dataVars.failedBlocks;
+    winston.info("Checking for past processing...");
+    if (fs.existsSync(this.dataStore)) {
+      let dataVars = JSON.parse(fs.readFileSync(this.dataStore,'utf8'));
+      // explicitly add them to the class variables 
+      if (dataVars.currentBlock)
+        this.currentBlock = dataVars.currentBlock;
+      if (dataVars.failedBlocks)
+        this.failedBlocks = dataVars.failedBlocks;
 
-          winston.info("Loaded previous state");
-          winston.info("Blocks processed:" + this.currentBlock);
-        }
-        else  
-          winston.info("No previous state found. Starting from scratch...");
+      winston.info("Loaded previous state");
+      winston.info("Blocks processed:" + this.currentBlock);
     }
+    else  
+      winston.info("No previous state found. Starting from scratch...");
+  }
 
-    /**
-     * This builds the signature hash, given a transaction object.
-     * We build this manually to deal with EIP155 and to improve efficiency
-     * in many calculations
-     * @param - transaction object
-     */
-    _buildSignatureHash(transaction) {
-      var rawSig = []
-      rawSig[0] = util.toBuffer(new BN(transaction.nonce));
-      rawSig[1] = util.toBuffer(new BN(transaction.gasPrice));
-      rawSig[2] = util.toBuffer(new BN(transaction.gas));
-      rawSig[3] = util.toBuffer(transaction.to);
-      rawSig[4] = util.toBuffer(new BN(transaction.value));
-      rawSig[5] = util.toBuffer(transaction.input);
-      rawSig[6] = util.toBuffer(transaction.v);
-      rawSig[7] = util.toBuffer(transaction.r);
-      rawSig[8] = util.toBuffer(transaction.s);
-      for (let i = 0, len= rawSig.length; i< len; i++){
-          if(rawSig[i].toString('hex') === '00' && i !=3 && i != 5){
-              rawSig[i] = Buffer.allocUnsafe(0)
-          }
-          if (i != 3 && i !=5) //Don't strip 0's from addresses or input values
-            rawSig[i] = util.stripZeros(rawSig[i])
+  /**
+   * This builds the signature hash, given a transaction object.
+   * We build this manually to deal with EIP155 and to improve efficiency
+   * in many calculations
+   * @param - transaction object
+   */
+  _buildSignatureHash(transaction) {
+    var rawSig = []
+    rawSig[0] = util.toBuffer(new BN(transaction.nonce));
+    rawSig[1] = util.toBuffer(new BN(transaction.gasPrice));
+    rawSig[2] = util.toBuffer(new BN(transaction.gas));
+    rawSig[3] = util.toBuffer(transaction.to);
+    rawSig[4] = util.toBuffer(new BN(transaction.value));
+    rawSig[5] = util.toBuffer(transaction.input);
+    rawSig[6] = util.toBuffer(transaction.v);
+    rawSig[7] = util.toBuffer(transaction.r);
+    rawSig[8] = util.toBuffer(transaction.s);
+    for (let i = 0, len= rawSig.length; i< len; i++){
+      if(rawSig[i].toString('hex') === '00' && i !=3 && i != 5){
+        rawSig[i] = Buffer.allocUnsafe(0)
       }
-      var msg = undefined
-      // EIP155 check the value of v
-      if (parseInt(transaction.v) > 28) {
-        rawSig[6] = util.toBuffer(parseInt(this.chainId));
-        rawSig[7] = Buffer.allocUnsafe(0);
-        rawSig[8] = Buffer.allocUnsafe(0);
-        msg = rawSig.slice()
-      } else {
-        msg = rawSig.slice(0, 6)
-      }
-      return util.rlphash(msg);
+      if (i != 3 && i !=5) //Don't strip 0's from addresses or input values
+        rawSig[i] = util.stripZeros(rawSig[i])
     }
-
-    /**
-     * Process an individual transaction
-     * this returns an address and its public key given a transaction
-     */
-     _processTransaction(transaction) {
-
-        let sigHash = this._buildSignatureHash(transaction);
-        let v = parseInt(transaction.v)
-        if (v > 28){
-          v -= this.chainId * 2 + 8
-        }
-        let pubKey = util.ecrecover(sigHash,v,transaction.r,transaction.s)
-        let derivedAddress = util.bufferToHex(util.publicToAddress(pubKey))
-
-        assert(derivedAddress.toLowerCase() === transaction.from.toLowerCase())
-        return {
-            'address': transaction.from,
-            'pubKey': pubKey.toString('hex'),
-        }
+    var msg = undefined
+    // EIP155 check the value of v
+    if (parseInt(transaction.v) > 28) {
+      rawSig[6] = util.toBuffer(parseInt(this.chainId));
+      rawSig[7] = Buffer.allocUnsafe(0);
+      rawSig[8] = Buffer.allocUnsafe(0);
+      msg = rawSig.slice()
+    } else {
+      msg = rawSig.slice(0, 6)
     }
+    return util.rlphash(msg);
+  }
 
-    /**
-     * Sends the public keys to the api. 
-     * This performs a bulk upload of all the transactions in the block currently
-     * being processed. We only care about addresses we haven't seen before. 
-     * @param pkList - A list of objects containing a public key and their
-     * associated Ethereum address  */
-    async _sendPktoAPI(pkObj, blockNumber) {
+  /**
+   * Process an individual transaction
+   * this returns an address and its public key given a transaction
+   */
+  _processTransaction(transaction) {
+
+    let sigHash = this._buildSignatureHash(transaction);
+    let v = parseInt(transaction.v)
+    if (v > 28){
+      v -= this.chainId * 2 + 8
+    }
+    let pubKey = util.ecrecover(sigHash,v,transaction.r,transaction.s)
+    let derivedAddress = util.bufferToHex(util.publicToAddress(pubKey))
+
+    assert(derivedAddress.toLowerCase() === transaction.from.toLowerCase())
+    return {
+      'address': transaction.from,
+      'pubKey': pubKey.toString('hex'),
+    }
+  }
+
+  /**
+   * Sends the public keys to the api. 
+   * This performs a bulk upload of all the transactions in the block currently
+   * being processed. We only care about addresses we haven't seen before. 
+   * @param pkList - A list of objects containing a public key and their
+   * associated Ethereum address  */
+   _sendPktoAPI(pkObj, blockNumber) {
+    return new Promise(function(resolve, reject) { 
       let putObj = JSON.stringify({
         key: this.apiKey,
         addresses: pkObj.addresses,
         pubkeys: pkObj.pubkeys
       })
       let headers = {
-          'Content-Type': 'application/json',
-          'Content-Length': putObj.length
+        'Content-Type': 'application/json',
+        'Content-Length': putObj.length
       };
-
+    
       var callback = (res) => { 
         if (res.statusCode != 200) { 
+          winston.error("API Failed. Status code:" + res.statusCode)
           this.failedBlocks.push(blockNumber)
-          return
+          reject()
         }
         res.setEncoding('utf8');
         let returnData='';
@@ -233,90 +235,105 @@ class PublicKeyCollector {
           if (returnData.result != 'success') {
             winston.error("API: Storage of Key Failed\n" + returnData.result)
             this.failedBlocks.push(blockNumber)
+            reject()
           }
+          resolve()
         })
       }
       https.request({...this.apiOptions, headers: headers}, callback).write(putObj)
-    }
+    }.bind(this));
+  }
 
-    /* Update any statistics we want to store. Currently just the successful
-     * block number 
-     */
-    _updateStats() {
-      let stats = JSON.stringify({ currentBlock: this.currentBlock, failedBlocks: this.failedBlocks});
-      fs.writeFileSync(this.dataStore, stats);
-    }
+  /* Update any statistics we want to store. Currently just the successful
+   * block number 
+   */
+  _updateStats() {
+    let stats = JSON.stringify({ currentBlock: this.currentBlock, failedBlocks: this.failedBlocks});
+    fs.writeFileSync(this.dataStore, stats);
+  }
 
-    /**
-     * This batch processes blocks until it reaches the current block height.
-     * The transactions within each block are processed and the public keys
-     * are sent to the api.
-     */
-    async processBlocks() {
-      while(this.currentBlock < this.lastBlock) { 
-        if(this.currentBlock % 1000 == 0 && this.currentBlock != 0){
-          winston.info(`Transactions Processed (last 1000 blocks): ${this.transactionTally}`)
-          this.transactionTally = 0;
-          winston.info(`Processing Block ${this.currentBlock}`)
-        }
-        let blockData = await this.web3.eth.getBlock(this.currentBlock, true)
-        if(blockData.transactions.length != 0) {
-          let pkObj = {addresses: [], pubkeys: []}
-          for (let i = 0; i < blockData.transactions.length; i++) {
-            // Only process transactions we haven't seen before.
-            if (blockData.transactions[i].nonce == 0) {
-              this.transactionTally += 1;
-              let pkEntry = this._processTransaction(blockData.transactions[i]);
-              pkObj.addresses.push(pkEntry.address)  
-              pkObj.pubkeys.push(pkEntry.pubKey)  
-            }
+  /**
+   * This batch processes blocks until it reaches the current block height.
+   * The transactions within each block are processed and the public keys
+   * are sent to the api.
+   */
+  async processBlocks() {
+    while(this.currentBlock < this.lastBlock) { 
+      if(this.currentBlock % 1000 == 0 && this.currentBlock != 0){
+        winston.info(`Transactions Processed (last 1000 blocks): ${this.transactionTally}`)
+        this.transactionTally = 0;
+        winston.info(`Processing Block ${this.currentBlock}`)
+      }
+      let blockData = await this.web3.eth.getBlock(this.currentBlock, true)
+      if(blockData.transactions.length != 0) {
+        let pkObj = {addresses: [], pubkeys: []}
+        for (let i = 0; i < blockData.transactions.length; i++) {
+          // Only process transactions we haven't seen before.
+          if (blockData.transactions[i].nonce == 0) {
+            this.transactionTally += 1;
+            let pkEntry = this._processTransaction(blockData.transactions[i]);
+            pkObj.addresses.push(pkEntry.address)  
+            pkObj.pubkeys.push(pkEntry.pubKey)  
           }
-          if (pkObj.addresses.length > 0) 
-            this._sendPktoAPI(pkObj, this.currentBlock) // don't wait for the api calls
-
-          this._updateStats();
-
-          if (this.currentBlock >= this.lastBlock)
-            break;
-          // Exit the loop if we are only updating a single block
-          if (this.updateMode)
-            break;
         }
-
-        // Lets process the next block
-        this.currentBlock++;
-        // If we are approaching the last block
-        if (this.lastBlock - this.currentBlock < 5) {
-          let blockNumber = await this.web3.eth.getBlockNumber()
-          this.lastBlock = blockNumber;
+        if (pkObj.addresses.length > 0)  {
+          // batch api requests. 
+          let batchSize = 200;
+          if (pkObj.addresses.length > batchSize) {
+            while (pkObj.addresses.length > 0) {
+              let tmpPkObj = { addresses: pkObj.addresses.splice(0,batchSize), pubkeys: pkObj.pubkeys.splice(0,batchSize) } 
+              await this._sendPktoAPI(tmpPkObj, this.currentBlock) // wait for these api calls
+            }
+          } 
+          else  // Don't batch
+          {
+            this._sendPktoAPI(pkObj, this.currentBlock) // don't wait for the api calls
+          }
         }
       }
+
+      this._updateStats();
+
+      if (this.currentBlock >= this.lastBlock)
+        break;
+      // Exit the loop if we are only updating a single block
+      if (this.updateMode)
+        break;
+
+      // Lets process the next block
+      this.currentBlock++;
+      // If we are approaching the last block
+      if (this.lastBlock - this.currentBlock < 5) {
+        let blockNumber = await this.web3.eth.getBlockNumber()
+        this.lastBlock = blockNumber;
+      }
     }
+  }
 
-    /**
-     * This updates the database as blocks are found.
-     * This is designed for perpetual real-time updates of the
-     * database
-     */
-     continualUpdate() {
-         winston.info("Starting the continual update...")
-         this.updateMode = true;
-         this.web3.eth.subscribe('newBlockHeaders')
-         .on('data', (blockHeader) => {
-             winston.info(`Processing Block: ${blockHeader.number}`)
-             this.currentBlock = blockHeader.number;
-             this.lastBlock = blockHeader.number;
-             this.processBlocks(); // Process this new block
-         })
-     }
+  /**
+   * This updates the database as blocks are found.
+   * This is designed for perpetual real-time updates of the
+   * database
+   */
+  continualUpdate() {
+    winston.info("Starting the continual update...")
+    this.updateMode = true;
+    this.web3.eth.subscribe('newBlockHeaders')
+      .on('data', (blockHeader) => {
+        winston.info(`Processing Block: ${blockHeader.number}`)
+        this.currentBlock = blockHeader.number;
+        this.lastBlock = blockHeader.number;
+        this.processBlocks(); // Process this new block
+      })
+  }
 
-     syncEmitter() {
-         return this.web3.eth.subscribe('syncing')
-         }
+  syncEmitter() {
+    return this.web3.eth.subscribe('syncing')
+  }
 
-     isSyncing() {
-       return this.web3.eth.isSyncing();
-     }
+  isSyncing() {
+    return this.web3.eth.isSyncing();
+  }
 }
 
 // execute the main function
